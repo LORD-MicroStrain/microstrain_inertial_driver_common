b980904e64526e31f6c0510fe8e78a19351885de
robbiefish <robbie.fish76@gmail.com>
2021-08-18 12:28:36 -0400

    Adds some logging to cmake file and allows user to specify an existing install of MSCL

--- cmake/download_mscl.cmake
+++ cmake/download_mscl.cmake
@@ -6,46 +6,60 @@ function(download_mscl)
     ""
     ${ARGN}
   )
-  # Allow the MSCL version to be overridden
-  set(MSCL_VERSION "${download_mscl_VERSION}" CACHE STRING "Version of MSCL to download and build with")
 
-  # Convert the CMake architecture to the format that MSCL uses
-  if("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "x86_64")
-    set(MSCL_ARCH "amd64")
-  elseif("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "aarch64")
-    set(MSCL_ARCH "arm64")
-  elseif("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "armhf")
-    set(MSCL_ARCH "armhf")
+  # If the user specified the location for MSCL we will not download the artifact
+  set(MSCL_DIR "" CACHE STRING "Path to MSCL that already exists on the system. If not specified, MSCL will be downloaded")
+  if(MSCL_DIR STREQUAL "")
+    # Allow the MSCL version to be overridden
+    set(MSCL_VERSION "${download_mscl_VERSION}" CACHE STRING "Version of MSCL to download and build with")
+
+    # Convert the CMake architecture to the format that MSCL uses
+    if("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "x86_64")
+      set(MSCL_ARCH "amd64")
+    elseif("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "aarch64")
+      set(MSCL_ARCH "arm64")
+    elseif("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "armhf")
+      set(MSCL_ARCH "armhf")
+    else()
+      message(FATAL_ERROR "Unsupported architecture: ${CMAKE_SYSTEM_PROCESSOR}")
+    endif()
+
+    # Install MSCL from github
+    set(MSCL_GITHUB_ORG "LORD-MicroStrain")
+    set(MSCL_GITHUB_REPO "MSCL")
+    set(MSCL_GITHUB_TAG "v${MSCL_VERSION}")
+    set(MSCL_GITHUB_ARTIFACT "c++-mscl_${MSCL_VERSION}_${MSCL_ARCH}.deb")
+    set(MSCL_DEB_ARTIFACT "${PROJECT_BINARY_DIR}/${MSCL_GITHUB_ARTIFACT}")
+
+    # If the artifact already exists, no need to download it again
+    if(NOT EXISTS "${MSCL_DEB_ARTIFACT}")
+      # Get the release ID for the version we are trying to download
+      message(STATUS "Looking for MSCL tag ${MSCL_GITHUB_TAG} on github")
+      set(MSCL_LIST_REPO_URL "https://api.github.com/repos/${MSCL_GITHUB_ORG}/${MSCL_GITHUB_REPO}/releases/tags/${MSCL_GITHUB_TAG}")
+      execute_process(
+        COMMAND ${CMAKE_COMMAND} -E env /bin/bash -c "curl -fsSl '${MSCL_LIST_REPO_URL}' | jq '.assets[] | select(.name==\"${MSCL_GITHUB_ARTIFACT}\") | .url' | sed 's/\"// g'"
+        OUTPUT_VARIABLE MSCL_DOWNLOAD_LINK
+      )
+
+      # Download the deb file
+      message(STATUS "Downloading ${MSCL_GITHUB_ARTIFACT} to ${MSCL_DEB_ARTIFACT}")
+      execute_process(
+        COMMAND ${CMAKE_COMMAND} -E env /bin/bash -c "curl -fsSLJo ${MSCL_DEB_ARTIFACT} -H 'Accept: application/octet-stream' ${MSCL_DOWNLOAD_LINK}"
+      )
+    endif()
+
+    # Extract the deb file into the workspace
+    get_filename_component(MSCL_DEB_EXTRACTED_DIR_NAME "${MSCL_GITHUB_ARTIFACT}" NAME_WE)
+    set(MSCL_DEB_EXTRACTED_DIR "${PROJECT_BINARY_DIR}/${MSCL_DEB_EXTRACTED_DIR_NAME}")
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E env /bin/bash -c "dpkg-deb -x '${MSCL_DEB_ARTIFACT}' '${MSCL_DEB_EXTRACTED_DIR}'"
+    )
+    set(MSCL_DIR "${MSCL_DEB_EXTRACTED_DIR}/usr/share/c++-mscl")
   else()
-    message(FATAL_ERROR "Unsupported architecture: ${CMAKE_SYSTEM_PROCESSOR}")
+    message(STATUS "Skipping download of MSCL and looking in ${MSCL_DIR} instead")
   endif()
 
-  # Install MSCL from github
-  set(MSCL_GITHUB_ORG "LORD-MicroStrain")
-  set(MSCL_GITHUB_REPO "MSCL")
-  set(MSCL_GITHUB_TAG "v${MSCL_VERSION}")
-  set(MSCL_GITHUB_ARTIFACT "c++-mscl_${MSCL_VERSION}_${MSCL_ARCH}.deb")
-
-  # Get the release ID for the version we are trying to download
-  set(MSCL_LIST_REPO_URL "https://api.github.com/repos/${MSCL_GITHUB_ORG}/${MSCL_GITHUB_REPO}/releases/tags/${MSCL_GITHUB_TAG}")
-  execute_process(
-    COMMAND ${CMAKE_COMMAND} -E env /bin/bash -c "curl -fsSl '${MSCL_LIST_REPO_URL}' | jq '.assets[] | select(.name==\"${MSCL_GITHUB_ARTIFACT}\") | .url' | sed 's/\"// g'"
-    OUTPUT_VARIABLE MSCL_DOWNLOAD_LINK
-  )
-
-  # Download the deb file
-  set(MSCL_DEB_ARTIFACT "${PROJECT_BINARY_DIR}/${MSCL_GITHUB_ARTIFACT}")
-  execute_process(
-    COMMAND ${CMAKE_COMMAND} -E env /bin/bash -c "curl -fsSLJo ${MSCL_DEB_ARTIFACT} -H 'Accept: application/octet-stream' ${MSCL_DOWNLOAD_LINK}"
-  )
-
-  # Extract the deb file into the workspace
-  get_filename_component(MSCL_DEB_EXTRACTED_DIR_NAME "${MSCL_GITHUB_ARTIFACT}" NAME_WE)
-  set(MSCL_DEB_EXTRACTED_DIR "${PROJECT_BINARY_DIR}/${MSCL_DEB_EXTRACTED_DIR_NAME}")
-  execute_process(
-    COMMAND ${CMAKE_COMMAND} -E env /bin/bash -c "dpkg-deb -x '${MSCL_DEB_ARTIFACT}' '${MSCL_DEB_EXTRACTED_DIR}'"
-  )
-  set(MSCL_DIR "${MSCL_DEB_EXTRACTED_DIR}/usr/share/c++-mscl")
+  # Set some variables in the parent scope
   set(MSCL_DIR "${MSCL_DIR}" PARENT_SCOPE)
 
   # Locate MSCL and the Boost install it was distributed with
