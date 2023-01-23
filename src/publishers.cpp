c5c363596c2f992d8e6887c8a661d1e74efab040
Rob Fisher <robbie.fish76@gmail.com>
2023-01-23 19:01:42 +0000

    Adds ability to report relative position from basestation

--- src/publishers.cpp
+++ src/publishers.cpp
@@ -966,7 +966,7 @@ void Publishers::setGpsTime(RosTimeType* time, const mip::data_shared::GpsTimest
   double subseconds = modf(timestamp.tow, &seconds);
 
   // Seconds since start of Unix time = seconds between 1970 and 1980 + number of weeks since 1980 * number of seconds in a week + number of complete seconds past in current week - leap seconds since start of GPS time
-  const uint64_t utc_milliseconds = static_cast<uint64_t>((315964800 + timestamp.week_number * 604800 + static_cast<uint64_t>(seconds) - 18) * 1000L) + static_cast<uint64_t>(std::round(subseconds * 1000.0));
+  const uint64_t utc_milliseconds = static_cast<uint64_t>((315964800 + timestamp.week_number * 604800 + static_cast<uint64_t>(seconds) - GPS_LEAP_SECONDS) * 1000L) + static_cast<uint64_t>(std::round(subseconds * 1000.0));
   setRosTime(time, utc_milliseconds / 1000, (utc_milliseconds % 1000) * 1000);
 }
 
